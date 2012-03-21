627a95e0f73ff6b5e93a2f0cb06e5d0c2784df51
jmoore <josh@glencoesoftware.com>
2012-03-21 16:27:41 +0000

    zeroc-ice33 3.3.1-clang

--- Library/Formula/zeroc-ice33.rb
+++ Library/Formula/zeroc-ice33.rb
@@ -3,19 +3,10 @@ require 'formula'
 class ZerocIce33 < Formula
   homepage 'https://github.com/joshmoore/zeroc-ice'
 
-  # FROM github:
-  # ------------
-  # url 'https://github.com/joshmoore/zeroc-ice/tarball/Ice-3.3.1'
-  # sha1 '1db95b5b168207b9d3fb8d03097440c5f5238f58'
-  # md5 '7d8fab84c67272cde9fa7d3529d27caf'
-  #
-  # def install:
-  #   system "mkdir cpp/lib cpp/bin"
-  #   ...
+  url 'https://nodeload.github.com/joshmoore/zeroc-ice/tarball/v.3.3.1-clang'
+  md5 '2ea5e68cbcf91121c0c196f6096aff9a'
+  sha1 '9055faf76ec7657e05036b7740d60bdc38e04f4a'
 
-  url 'http://www.zeroc.com/download/Ice/3.3/Ice-3.3.1.tar.gz'
-  md5 '1f37dfcec4662fcde030553fb447d064'
-  sha1 '08d0f32bdb7d909b4a531d45cccfa97bdde649aa'
 
   head 'git://github.com/joshmoore/zeroc-ice.git', :tag => 'Ice-3.3.1'
 
