3626f7c52a17c01dc7db0197185b03c942c1f1ed
Sebastien Besson <seb.besson@gmail.com>
2012-06-27 16:20:38 +0100

    Create a formula for 4.3.4 as brew versions does not work with tap

---
+++ Formula/omero43.rb
@@ -0,0 +1,59 @@
+require 'formula'
+
+class Omero < Formula
+  homepage 'https://www.openmicroscopy.org'
+
+  url 'https://github.com/openmicroscopy/openmicroscopy/tarball/v.4.3.4'
+  md5 'c5b32ba1452ae2e038c1fc9b5760c811'
+  sha1 '2cb765f6b2de3a208ea5df5847473bf27056e830'
+  version '4.3.4'
+
+  depends_on 'mplayer'
+  depends_on 'zeroc-ice33'
+
+  def options
+    [
+      ["--with-cpp", "Build OmeroCpp libraries."]
+    ]
+  end
+
+  def install
+    args = ["./build.py", "-Dice.home=#{HOMEBREW_PREFIX}", "-Ddist.dir=#{prefix}"]
+    if ARGV.include? '--with-cpp'
+        args << 'build-all'
+    else
+        args << 'build-default'
+    end
+    system *args
+    ice_link
+  end
+
+  def ice_link
+    ohai "Linking zeroc libaries"
+    python = lib+"python"
+
+    zeroc = Formula.factory('zeroc-ice33')
+    zp = zeroc.prefix+"python"
+    zp.cd { Dir["*"].each {|p| ln_sf zp + p, python + File.basename(p) }}
+
+  end
+
+  def caveats; <<-EOS.undent
+
+    For non-homebrew Python, you need to set your PYTHONPATH:
+    export PYTHONPATH=#{lib}/python:$PYTHONPATH
+
+    EOS
+  end
+
+  def test
+    mktemp do
+      (Pathname.pwd/'test.py').write <<-EOS.undent
+        #!/usr/bin/env python
+        import Ice
+        EOS
+
+      system "python", "test.py"
+    end
+  end
+end
