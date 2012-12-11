60f7d50301cf273b265a4ba5f2731a2b7c982f59
jmoore <josh@glencoesoftware.com>
2012-12-11 09:38:15 +0100

    Add 0.1.0 tag support

--- Formula/scc.rb
+++ Formula/scc.rb
@@ -2,11 +2,17 @@ require 'formula'
 
 class Scc < Formula
   homepage 'https://github.com/snoopycrimecop/snoopycrimecop'
-
   head 'https://github.com/snoopycrimecop/snoopycrimecop.git', :branch => 'master'
+  url 'https://github.com/snoopycrimecop/snoopycrimecop/archive/0.1.0.tar.gz'
+  version '0.1.0'
+  sha1 '08c6349db028be5287eb7d410c697a67ada6ad38'
 
   def install
     File.rename("scc.py", "scc")
     bin.install("scc")
+  end
+
+  def test
+    system "scc -h"
   end
 end
