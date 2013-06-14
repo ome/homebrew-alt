a458a5a5de4c964f559231a5e67d1589a038502e
Sebastien Besson <seb.besson@gmail.com>
2013-06-14 13:48:39 +0100

    Fix formula audit

--- Formula/zeroc-ice33.rb
+++ Formula/zeroc-ice33.rb
@@ -6,7 +6,7 @@ class ZerocIce33 < Formula
   url 'https://github.com/joshmoore/zeroc-ice/archive/v.3.3.1-clang.tar.gz'
   sha1 'd6bac03c32a27fe1414c519d3e6e08a2156232b6'
 
-  head 'http://github.com/joshmoore/zeroc-ice.git', :branch => 'Ice-3.3.1'
+  head 'https://github.com/joshmoore/zeroc-ice.git', :branch => 'Ice-3.3.1'
 
   depends_on 'mcpp'
   depends_on 'berkeley-db46' => '--without-java'
