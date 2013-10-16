61511228fa0d9812e891dfca3217ceca80d7eba2
Sebastien Besson <seb.besson@gmail.com>
2013-10-16 16:12:07 +0100

    Update OMERO formula to 4.4.9

--- Formula/omero.rb
+++ Formula/omero.rb
@@ -4,8 +4,7 @@ class Omero < Formula
   homepage 'http://www.openmicroscopy.org/site/products/omero'
 
   head 'https://github.com/openmicroscopy/openmicroscopy.git', :branch => 'dev_4_4'
-  url 'https://github.com/openmicroscopy/openmicroscopy.git', :tag => 'v.4.4.8p1'
-  version '4.4.8p1'
+  url 'https://github.com/openmicroscopy/openmicroscopy.git', :tag => 'v.4.4.9'
 
   devel do
     url 'https://github.com/openmicroscopy/openmicroscopy.git', :tag => 'v.5.0.0-beta1'
@@ -117,22 +116,23 @@ index efa9e62..976df1a 100644
              </try>
              <catch>
 -                <echo>UNKNOWN</echo>
-+                <echo>4.4.8p1</echo>
++                <echo>4.4.9</echo>
              </catch>
          </trycatch>
          </sequential>
+
 diff --git a/components/bioformats/ant/common.xml b/components/bioformats/ant/common.xml
-index c2760a2..eeffb5d 100644
+index 149bb2d..a8ed9a7 100644
 --- a/components/bioformats/ant/common.xml
 +++ b/components/bioformats/ant/common.xml
-@@ -49,6 +49,10 @@ Type "ant -p" for a list of targets.
-         <propertyregex property="vcs.date"
-           input="${git.info}" regexp="Date: +([^\n]*)" select="\1"/>
-       </then>
-+      <else>
-+        <property name="vcs.revision" value="660f607f71"/>
-+        <property name="vcs.date" value="Wed May 1 06:57:13 2013 -0700"/>
-+      </else>
-     </if>
- 
-     <!-- set release version from repository URL -->
+@@ -107,7 +107,9 @@ Type "ant -p" for a list of targets.
+    </if>
+
+    <!-- set release version by default if nothing is set -->
+-    <property name="release.version" value="UNKNOWN"/>
++    <property name="vcs.revision" value="22bff853fa"/>
++    <property name="vcs.date" value="Tue Oct 15 09:11:40 2013 -0700"/>
++    <property name="release.version" value="4.4.9"/>
+  </target>
+
+</project>
