2ec90b0d4209864ad505bb482aa0e2fde1d607a3
Sebastien Besson <seb.besson@gmail.com>
2014-02-25 21:00:29 +0000

    Update Bio-Formats formula to 5.0.0
    - Remove without-ome-tools option
    - Use source zip from downloads page and remove patches
    - Remove devel version of the formula

--- Formula/bioformats.rb
+++ Formula/bioformats.rb
@@ -3,48 +3,22 @@ require 'formula'
 class Bioformats < Formula
   homepage 'http://www.openmicroscopy.org/site/products/bio-formats'
 
-  head 'https://github.com/openmicroscopy/bioformats.git', :branch => 'dev_4_4'
-  url 'https://github.com/openmicroscopy/bioformats/archive/v4.4.10.tar.gz'
-  sha1 '875c552aea03107d5e4e9b55320b0abf5918effa'
+  url 'http://downloads.openmicroscopy.org/bio-formats/5.0.0/artifacts/bioformats-5.0.0.zip'
+  sha1 'f850db7164e8c9576d9782d6ad65d17c544719a7'
 
-  devel do
-    url 'https://github.com/openmicroscopy/bioformats/archive/v5.0.0-rc2.tar.gz'
-    version '5.0.0-rc2'
-    sha1 '5029a721925a990692163a9bfd3cdf6b49be2fd0'
-  end
-
-  depends_on :python if build.devel?
-  depends_on 'genshi' => :python if build.devel?
-  option 'without-ome-tools', 'Do not build OME Tools.'
-
-  def patches
-    # build generates a version number with 'git show' command
-    # but Homebrew build runs in temp copy created via git checkout-index,
-    # so 'git show' does not work.
-    if build.devel?
-      {:p1 => "https://gist.github.com/sbesson/8973435/raw/000c0cf3fb85c7f4d622fdcdd87917bd9daaa379/version.xml.patch"}
-    elsif not build.head?
-      DATA if not (build.head? or build.devel?)
-    end
-  end
+  depends_on :python
+  depends_on 'genshi' => :python
 
   def install
     # Build libraries
     args = ["ant", "clean" ,"tools", "utils"]
-    if not build.include? 'without-ome-tools'
-        args << 'tools-ome'
-    end
     system *args
 
     # Remove Windows files
     rm Dir["tools/*.bat"]
 
     # Copy artifacts
-    bin.install Dir["artifacts/loci_tools.jar"]
-    if not build.include? 'without-ome-tools'
-      bin.install Dir["artifacts/ome_tools.jar"]
-      bin.install Dir["artifacts/ome-io.jar"]
-    end
+    bin.install Dir["artifacts/bioformats_package.jar"]
 
     # Copy command line-tools
     bin.install Dir["tools/*"]
@@ -53,20 +27,3 @@ class Bioformats < Formula
     system "showinf", "-version"
   end
 end
-
-__END__
-diff --git a/ant/common.xml b/ant/common.xml
-index 149bb2d..a8ed9a7 100644
---- a/ant/common.xml
-+++ b/ant/common.xml
-@@ -107,7 +107,9 @@ Type "ant -p" for a list of targets.
-     </if>
- 
-     <!-- set release version by default if nothing is set -->
--    <property name="release.version" value="UNKNOWN"/>
-+    <property name="vcs.revision" value="d853f51"/>
-+    <property name="vcs.date" value="Tue Jan 14 11:59:30 2014 -0800"/>
-+    <property name="release.version" value="4.4.10"/>
-   </target>
- 
- </project>
