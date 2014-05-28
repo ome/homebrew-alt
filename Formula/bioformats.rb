a4ad00d1f6785f9298db6b23f214c6998db09d29
Sebastien Besson <seb.besson@gmail.com>
2014-05-28 14:02:12 +0100

    Bump OMERO and Bio-Formats versions to 5.0.2

--- Formula/bioformats.rb
+++ Formula/bioformats.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Bioformats < Formula
   homepage 'http://www.openmicroscopy.org/site/products/bio-formats'
 
-  url 'http://downloads.openmicroscopy.org/bio-formats/5.0.1/artifacts/bioformats-5.0.1.zip'
-  sha1 '32e1a3ea1d755c0fd6c93bfbb1f3d98d228725b8'
+  url 'http://downloads.openmicroscopy.org/bio-formats/5.0.2/artifacts/bioformats-5.0.2.zip'
+  sha1 '2b286741dec8a7b8ec4244f60aef410c07973194'
 
   depends_on :python
   depends_on 'genshi' => :python
