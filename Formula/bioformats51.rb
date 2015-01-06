7ed22241f83afdf3bbaa8bc5213977b0354009d7
Sebastien Besson <seb.besson@gmail.com>
2015-01-06 10:52:39 +0000

    Add Formulas for versions 5.1.0-m3 of OMERO and Bio-Formats

---
+++ Formula/bioformats51.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Bioformats51 < Formula
+  homepage 'http://www.openmicroscopy.org/site/products/bio-formats'
+
+  url 'http://downloads.openmicroscopy.org/bio-formats/5.1.0-m3/artifacts/bioformats-5.1.0-m3.zip'
+  sha1 '8072018dd751e30004d4929eb40e7450aa3d11fb'
+
+  depends_on :python => :build
+  depends_on :ant => :build
+  depends_on 'genshi' => :python
+
+  def install
+    # Build libraries
+    args = ["ant", "clean" ,"tools", "utils"]
+    system *args
+
+    # Remove Windows files
+    rm Dir["tools/*.bat"]
+
+    # Copy artifacts
+    bin.install "artifacts/bioformats_package.jar"
+
+    # Copy command line-tools
+    bin.install Dir["tools/*"]
+  end
+  test do
+    system "showinf", "-version"
+  end
+end
