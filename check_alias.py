bffeb64b0e9344b4fb19b74bbc92449a80e3a6a3
Sebastien Besson <seb.besson@gmail.com>
2015-01-14 09:45:55 +0000

    Add check_alias.py script and run it as part of Travis build
    This script should check the diff between the alias and the versioned
    OMERO and Bio-Formats formulas is only the class definition line.

---
+++ check_alias.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import difflib
+
+version = '50'
+
+
+def get_files_diff(file1, file2):
+    with open(file1) as f:
+        f1 = f.read().splitlines(1)
+
+    with open(file2) as f:
+        f2 = f.read().splitlines(1)
+
+    d = difflib.Differ()
+    r = d.compare(f1, f2)
+    return ([l for l in r if l.startswith('-')],
+            [l for l in r if l.startswith('+')])
+
+
+def get_expected_diff(formula, version):
+
+    uppercase_formula = formula[0].upper() + formula[1:]
+    return (['- class %s%s < Formula\n' % (uppercase_formula, version)],
+            ['- class %s < Formula\n' % uppercase_formula])
+
+
+for formula in ['omero', 'bioformats']:
+    file1 = 'Formula/%s%s.rb' % (formula, version)
+    file2 = 'Formula/%s.rb' % formula
+    difflines = get_files_diff(file1, file2)
+    assert difflines == get_expected_diff(formula, version)
