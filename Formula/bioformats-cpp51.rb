ebbdc111ce887d039010b81207264f4c369824fa
Roger Leigh <r.leigh@dundee.ac.uk>
2015-03-17 19:36:07 +0000

    Package Bio-Formats C++ library and tests

---
+++ Formula/bioformats-cpp51.rb
@@ -0,0 +1,62 @@
+require 'formula'
+
+class BioformatsCpp51 < Formula
+  homepage "http://www.openmicroscopy.org/site/products/bio-formats"
+  url "http://downloads.openmicroscopy.org/bio-formats/5.1.0-m4/artifacts/bioformats-5.1.0-m4.zip"
+  sha1 "07c760b9cabab022f5b90a31ffc699149ba2d014"
+  head "https://github.com/openmicroscopy/bioformats.git"
+
+  option "without-check", "Skip build time tests (not recommended)"
+  option "with-qt5", "Build with Qt5 (used for OpenGL image rendering)"
+  option "with-docs", "Build API reference and manual pages"
+
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
+  depends_on "libpng" => :build
+  depends_on "libtiff" => :build
+  depends_on "xerces-c" => :build
+  depends_on "graphicsmagick" => :optional if build.with? "check"
+  depends_on "qt5" => :build if build.with? "qt5"
+  depends_on "glm" => :build if build.with? "qt5"
+  depends_on "doxygen" => :build if build.with? "docs"
+  depends_on "graphviz" => :build if build.with? "docs"
+
+  needs :cxx11
+
+  resource "sphinx" do
+    url "https://pypi.python.org/packages/source/S/Sphinx/Sphinx-1.2.3.tar.gz"
+    sha1 "3a11f130c63b057532ca37fe49c8967d0cbae1d5"
+  end
+
+  def install
+    if build.with? "docs"
+      ENV.prepend_create_path "PYTHONPATH", buildpath+"sphinx/lib/python2.7/site-packages"
+      ENV.prepend_path "PATH", buildpath+"sphinx/bin"
+    end
+    ENV.prepend_path "PATH", "#{HOMEBREW_PREFIX}/opt/qt5/bin" if build.with? "qt5"
+
+    # Override std_cmake_args; CMAKE_BUILD_TYPE must be one of
+    # [Hybrid, Pure][Debug, Release]
+    args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}",
+            "-DCMAKE_BUILD_TYPE=PureRelease",
+            "-DCMAKE_VERBOSE_MAKEFILE=ON",
+            "-Wno-dev"]
+
+    # Bundle tests & examples together for check because examples directory
+    # includes code that exercises Qt5 functionality (via spy plots)
+    args << "-Dtest=OFF" if build.without? "check"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make"
+
+      if build.with? "check"
+        # Basic smoke test of build for now
+        system "ctest", "-V"
+      end
+
+      system "make", "install"
+    end
+  end
+
+end
