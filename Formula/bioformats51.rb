7ad42aef663b4a47f5d935acc0471fe69c68af57
Sebastien Besson <seb.besson@gmail.com>
2016-05-06 15:42:25 +0100

    Fix the URL

--- Formula/bioformats51.rb
+++ Formula/bioformats51.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Bioformats51 < Formula
   homepage 'http://www.openmicroscopy.org/site/products/bio-formats'
 
-  url 'http://downloads.openmicroscopy.org/bio-formats/5.1.9/artifacts/bioformats-5.1.10.zip'
+  url 'http://downloads.openmicroscopy.org/bio-formats/5.1.10/artifacts/bioformats-5.1.10.zip'
   sha256 '4bae68c8e99c6df8dae699f331b1554e6be040b34e05ca47ae93bd77841b8bc2'
 
   depends_on :python => :build
