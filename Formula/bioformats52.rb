e5839c28fb053f90c37c287b11453990cdbd8f30
Sebastien Besson <seb.besson@gmail.com>
2016-09-13 22:04:25 +0100

    Fix warnings flagged by brew audit --script

--- Formula/bioformats52.rb
+++ Formula/bioformats52.rb
@@ -1,17 +1,15 @@
-require 'formula'
-
 class Bioformats52 < Formula
-  homepage 'http://www.openmicroscopy.org/site/products/bio-formats'
-
-  url 'http://downloads.openmicroscopy.org/bio-formats/5.2.2/artifacts/bioformats-5.2.2.zip'
-  sha256 'fbc01f12dd3d5d08f5dcf46fec7476a30c219fcd38937720d32c2c4a6625625d'
+  desc "Library for reading proprietary image file formats"
+  homepage "http://www.openmicroscopy.org/site/products/bio-formats"
+  url "http://downloads.openmicroscopy.org/bio-formats/5.2.2/artifacts/bioformats-5.2.2.zip"
+  sha256 "fbc01f12dd3d5d08f5dcf46fec7476a30c219fcd38937720d32c2c4a6625625d"
 
   depends_on :python => :build
   depends_on :ant => :build
 
   def install
     # Build libraries
-    args = ["ant", "clean" ,"tools", "utils"]
+    args = ["ant", "clean", "tools", "utils"]
     system *args
 
     # Remove Windows files
