8ddb535f75449ea35c7a3a25a6aeb57a3b5ae93e
David Gault <d.gault@dundee.ac.uk>
2017-02-01 19:34:52 +0000

    Bump Bio-Formats formula to 5.3.3

--- Formula/bioformats53.rb
+++ Formula/bioformats53.rb
@@ -1,8 +1,8 @@
 class Bioformats53 < Formula
   desc "Library for reading proprietary image file formats"
   homepage "http://www.openmicroscopy.org/site/products/bio-formats"
-  url "http://downloads.openmicroscopy.org/bio-formats/5.3.2/artifacts/bioformats-5.3.2.zip"
-  sha256 "f9dde792dc24f7cf27e558dda8d0f69dddfde4d41a4339f79e5bb5fd11793ea8"
+  url "http://downloads.openmicroscopy.org/bio-formats/5.3.2/artifacts/bioformats-5.3.3.zip"
+  sha256 "48236e427026a6c14db5a8086907481cfc94e5987e2d131607972e2d87aab381"
 
   depends_on "ant" => :build
 
