c8b1aed42e3bffe8ca09fadf553bad0181ab9e30
David Gault <d.gault@dundee.ac.uk>
2017-05-08 16:16:29 +0100

    Correct formula

--- Formula/bioformats55.rb
+++ Formula/bioformats55.rb
@@ -2,7 +2,7 @@ class Bioformats55 < Formula
   desc "Library for reading proprietary image file formats"
   homepage "http://www.openmicroscopy.org/site/products/bio-formats"
   url "http://downloads.openmicroscopy.org/bio-formats/5.5.0/artifacts/bioformats-5.5.0.zip"
-  sha256 "da0d3d92ec7a87952b400af4e15bc1b5908067b349941e8a7a468724378140b3b"
+  sha256 "a0d3d92ec7a87952b400af4e15bc1b5908067b349941e8a7a468724378140b3b"
 
   depends_on "ant" => :build
 
