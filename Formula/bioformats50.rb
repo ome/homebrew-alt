1bcb639429d02bc67d537aee1fd81720d4b8b7ef
David Gault <d.gault@dundee.ac.uk>
2018-04-26 21:31:47 +0100

    Update formulas to fix errors due to upstream changes

--- Formula/bioformats50.rb
+++ Formula/bioformats50.rb
@@ -6,8 +6,8 @@ class Bioformats50 < Formula
   url 'http://downloads.openmicroscopy.org/bio-formats/5.0.8/artifacts/bioformats-5.0.8.zip'
   sha256 'bc059541de55b027488aa5af784c5c807d65602409d3589b6367ffdfc00653fb'
 
-  depends_on :python => :build
-  depends_on :ant => :build
+  depends_on "python@2" => :build
+  depends_on "ant" => :build
 
   def install
     # Build libraries
