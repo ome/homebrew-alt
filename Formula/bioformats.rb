e4e4b334b8f1e3a185892f1b5052d25a39d1f3f8
Sébastien Besson <seb.besson@gmail.com>
2018-09-04 13:31:50 +0100

    Update checksum

--- Formula/bioformats.rb
+++ Formula/bioformats.rb
@@ -2,7 +2,7 @@ class Bioformats < Formula
   desc "Library for reading proprietary image file formats"
   homepage "https://www.openmicroscopy.org/site/products/bio-formats"
   url "https://downloads.openmicroscopy.org/bio-formats/5.9.2/artifacts/bioformats-5.9.2.zip"
-  sha256 "https://downloads.openmicroscopy.org/bio-formats/5.9.2/artifacts/bioformats-5.9.2.zip"
+  sha256 "7f6ccd612dfe53a6a53c637568f8153f3f83b356354c8851da0e6b46b0ae9c18"
 
   depends_on "ant" => :build
 
