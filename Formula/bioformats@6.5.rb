71c2b4c56a621bfa497f97203334ca1c87504d03
David Gault <d.gault@dundee.ac.uk>
2020-12-16 12:22:41 +0000

    Add formula for BF 6.5

---
+++ Formula/bioformats@6.5.rb
@@ -0,0 +1,32 @@
+class BioformatsAT65 < Formula
+  desc "Library for reading proprietary image file formats"
+  homepage "https://www.openmicroscopy.org/bio-formats"
+  url "https://downloads.openmicroscopy.org/bio-formats/6.5.1/artifacts/bioformats-6.5.1.zip"
+  sha256 "74c045283321430cfe639faf6b53fc1b561682d364290136d461e1776c819ef4"
+
+  depends_on "ant" => :build
+
+  def install
+    # Build libraries
+    args = ["ant", "clean", "tools", "-Dmaven.repo.local",
+            "#{buildpath}/.m2/repository"]
+    system *args
+
+    # Remove Windows files
+    rm Dir["tools/*.bat"]
+
+    # Copy artifacts
+    libexec.install "artifacts/bioformats_package.jar"
+
+    # Copy command line-tools
+    libexec.install Dir["tools/*"]
+
+    %w[bfconvert domainlist formatlist ijview mkfake omeul showinf tiffcomment xmlindent xmlvalid].each do |fn|
+      bin.install_symlink libexec/fn
+    end
+  end
+  test do
+    system "showinf", "-version"
+    system "bfconvert", "-version"
+  end
+end
