3115bdb5931ec7dd3e1e437d3c13216036f5beb4
David Gault <d.gault@dundee.ac.uk>
2022-10-13 14:01:45 +0100

    Bump Bio-Formats formula to 6.11.0

--- Formula/bioformats@6.10.rb
+++ Formula/bioformats@6.10.rb
@@ -1,8 +1,8 @@
-class Bioformats < Formula
+class BioformatsAT610 < Formula
   desc "Library for reading proprietary image file formats"
   homepage "https://www.openmicroscopy.org/bio-formats"
-  url "https://downloads.openmicroscopy.org/bio-formats/6.11.0/artifacts/bioformats-6.11.0.zip"
-  sha256 "4dd90d2285eb39ec8a7386e6fc590dc7b1d822226a3b76f71e4961a49cb5498c"
+  url "https://downloads.openmicroscopy.org/bio-formats/6.10.1/artifacts/bioformats-6.10.1.zip"
+  sha256 "4da7005c27dc7f8a98a81c8f84df196fd0db1af20e300bea55595e2e08ef93d5"
 
   depends_on "ant" => :build
 
